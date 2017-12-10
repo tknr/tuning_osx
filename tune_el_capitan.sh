b64b56d9a504a9b2847f632af6c5936721371cde
tknr <geishagirlskoume@gmail.com>
2017-12-10 14:01:28 +0900

    init

---
+++ tune_el_capitan.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+## http://www.webessentials.biz/osx-customize/firstelcapitan/
+
+defaults write com.apple.desktopservices DSDontWriteNetworkStores true
+defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
+defaults write -g QLPanelAnimationDuration -float 0
+defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
+defaults write com.apple.finder DisableAllAnimations -bool true
+defaults write com.apple.dock launchanim -bool false
+defaults write com.apple.Dock autohide-delay -float 0
+defaults write com.apple.dock expose-animation-duration -float 0.1
+defaults write com.apple.mail DisableReplyAnimations -bool true
+defaults write com.apple.mail DisableSendAnimations -bool true
+defaults write com.apple.Safari WebKitInitialTimedLayoutDelay 0.1
+defaults read -g net.inet.tcp.delayed_ack
+defaults read net.inet.tcp.delayed_ack
+defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
+defaults write -g QLPanelAnimationDuration -float 0
+defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
+defaults write com.apple.finder DisableAllAnimations -bool true
+defaults write com.apple.dock launchanim -bool false
+defaults write com.apple.Dock autohide-delay -float 0
+defaults write com.apple.dock expose-animation-duration -float 0.1
+defaults write com.apple.mail DisableReplyAnimations -bool true
+defaults write com.apple.mail DisableSendAnimations -bool true
+defaults write com.apple.Safari WebKitInitialTimedLayoutDelay 0.1
+defaults write -g NSWindowResizeTime -float 0.005
+defaults write com.apple.dock expose-animation-duration -float 0.05
+
+killall Dock
+killall Finder
